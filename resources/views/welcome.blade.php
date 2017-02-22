397c134ad4130200686b579a1cbf65d2d191ad88
Ana Perez <aperalb@gmail.com>
2017-02-22 17:14:51 +0100

    Time Brings Roses

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Time Brings Roses
                 </div>
 
                 <div class="links">
